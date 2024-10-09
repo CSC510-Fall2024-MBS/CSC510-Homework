b19762a7a7dce5fac347a1ef3e1fbb39838635b0
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-09 22:05:12 +0000

    Step 3 part a

--- task2.sh
+++ task2.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Use the following as references:
+#############
+#  Part a   #
+#############
 
-# Get all files that contain at least one instance of "CSC510"
-grep -c "CSC510" file*
+# Gets all files by word instead of line count
+# Get files containing sample by word | cut colon, take first half | files containing CSC510 by word | get unique counts |
+# | Filter out counts less than 3 | cut colon | cut whitespace from front 9 characters
+grep -o "sample" file* | cut -d: -f1 | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9] file*" | cut -d: -f1 | cut -b 9-
 
-# Get all files that contain one instance of "sample"
-grep -c "sample" file* | grep -E ":1$"
+#############
+#  Part b   #
+#############
 
-# Get every individual instance of the word "CSC510"
-grep -o "CSC510" file*
+
+
+
