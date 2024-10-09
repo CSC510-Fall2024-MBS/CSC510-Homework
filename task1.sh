14922987bf03a8a8efd896f569b00a63f8070423
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-09 03:45:18 +0000

    Added comments to task1.sh

--- task1.sh
+++ task1.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-#
+
+# Find PID of the process initialized by infinite.sh
 PID=$(pgrep -f infinite.sh)
+
+# Attempt to kill the process
 pkill -f infinite.sh
 
+# If successfully killed, then return a success message, otherwise
+# return a failure message and exit with failure status 1
 if kill -0 "$PID" 2>/dev/null; then 
-    echo " failed to kill process with PID $PID"
+    echo "Failed to kill process (infinite.sh) with PID: $PID"
     exit 1
 else 
-    echo "successfully killed process PID $PID"
+    echo "Successfully killed process (infinite.sh) PID: $PID"
 fi
 
