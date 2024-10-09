3544e9a0227c1c9d89cd8a30a82a711ff2f3934e
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-09 22:55:02 +0000

    Progress on 3b and 3c, but not done yet

--- task2.sh
+++ task2.sh
@@ -4,15 +4,21 @@
 #  Part a   #
 #############
 
-# Gets all files by word instead of line count
+# Gets all files by word instead of line count, this preserves the number of occurrences
 # Get files containing sample by word | cut colon, take first half | files containing CSC510 by word | get unique counts |
-# | Filter out counts less than 3 | cut colon | cut whitespace from front 9 characters
-grep -o "sample" file* | cut -d: -f1 | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9] file*" | cut -d: -f1 | cut -b 9-
+# | Filter out counts less than 3 | cut colon | cut whitespace from front 7 characters
+grep -o "sample" file* | cut -d: -f1 | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9] file*" | cut -d: -f1 | cut -b 7-
 
 #############
 #  Part b   #
 #############
 
+# Sort in descending order (still need to sort tiebreakers by filesize with gawk)
+sort -r
 
+#############
+#  Part c   #
+#############
 
-
+# Change filenames from file_ to filtered_ | print files
+sed 's/file_/filtered_/' | xargs ls
