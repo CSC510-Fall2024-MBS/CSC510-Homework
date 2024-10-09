60d83bff8b90c7d83c7d4966735e2a77aa29850d
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-09 05:39:33 +0000

    Removed extra unnecessary files and reference material for task2

--- task2.sh
+++ task2.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# Use the following as references:
+
+# Get all files that contain at least one instance of "CSC510"
+grep -c "CSC510" file*
+
+# Get all files that contain one instance of "sample"
+grep -c "sample" file* | grep -E ":1$"
+
+# Get every individual instance of the word "CSC510"
+grep -o "CSC510" file*
