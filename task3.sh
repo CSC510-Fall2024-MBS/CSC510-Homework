6f6bd63cbbc139a5873a41c7434dd517d08bd2ed
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-10 03:47:13 +0000

    Completed task 4 part a and b

--- task3.sh
+++ task3.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+#############
+#  Part a   #
+#############
+
+# Get all second-class passengers who embarked from Southampton (S)
+grep -E "^[0-9]{1,3},[0-9],2,.*,.*,.*,.*,.*,.*,.*,.*,S" titanic.csv
+
+#############
+#  Part b   #
+#############
+
+# Get all second-class passengers from Southampton | Change female and male to F and M respectively
+grep -E "^[0-9]{1,3},[0-9],2,.*,.*,.*,.*,.*,.*,.*,.*,S" titanic.csv | sed 's/female/F/; s/male/M/' titanic.csv
+
+#############
+#  Part c   #
+#############
+
+# It should theoretically print out the average age, but doesn't seem to work yet
+grep -E "^[0-9]{1,3},[0-9],2,".*",.*,.*,.*,.*,.*,.*,.*,S" titanic.csv | sed 's/female/F/; s/male/M/' | gawk '{ ageSum += $6; counter += 1 } END { print ageSum / counter }'
+
