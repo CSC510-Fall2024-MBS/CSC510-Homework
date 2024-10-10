9d05458ebd08f59b7388ddacc5e84cf464c7f552
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-10 05:33:41 +0000

    Trimmed task2 and added labels to task3

--- task2.sh
+++ task2.sh
@@ -7,14 +7,14 @@
 # Gets all files by word instead of line count, this preserves the number of occurrences
 # Get files containing sample by word | cut colon, take first half | files containing CSC510 by word | get unique counts |
 # | Filter out counts less than 3 | cut colon | cut whitespace from front 7 characters
-grep -o "sample" file* | cut -d: -f1 | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9] file*" | cut -d: -f1 | cut -b 7-
+grep -ol "sample" file* | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9]{1,} file*" | cut -d: -f1 | cut -b 7-
 
 #############
 #  Part b   #
 #############
 
 # Sort in descending order (still need to sort tiebreakers by filesize with gawk)
-sort -r
+sort -rk1,1
 
 #############
 #  Part c   #
@@ -22,3 +22,5 @@ sort -r
 
 # Change filenames from file_ to filtered_ | print files
 sed 's/file_/filtered_/' | xargs ls
+
+exit
