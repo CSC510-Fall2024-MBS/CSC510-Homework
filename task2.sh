5c23fd9282208c70f02c6f77d663774b67dd0b31
bhhuynh <108171861+brianhhuynh38@users.noreply.github.com>
2024-10-11 01:39:47 +0000

    Moved task2_try.sh into task2.sh and updated output so the .sh file properly runs and outputs

--- task2.sh
+++ task2.sh
@@ -4,23 +4,29 @@
 #  Part a   #
 #############
 
-# Gets all files by word instead of line count, this preserves the number of occurrences
+# Gets all files containing sample by word, then 
+# grep -ol "sample" dataset1/file* | xargs -I{} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then size=$(stat --printf="%s" "{}"); echo "$count $size $(basename "{}")"; fi'
+
+
+# [OUTDATED]
 # Get files containing sample by word | cut colon, take first half | files containing CSC510 by word | get unique counts |
 # | Filter out counts less than 3 | cut colon | cut whitespace from front 7 characters
-grep -ol "sample" file* | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9]{1,} file*" | cut -d: -f1 | cut -b 7-
+# grep -ol "sample" file* | xargs grep -o "CSC510" | uniq -c | grep -E "[3-9]{1,} file*" | cut -d: -f1 | cut -b 7-
 
 #############
 #  Part b   #
 #############
 
-# Sort in descending order (still need to sort tiebreakers by filesize with gawk)
-sort -rk1,1
+# Sort in descending order and by filesize to break tiebreakers
+# sort -k1,1nr -k2,2nr
 
 #############
 #  Part c   #
 #############
 
 # Change filenames from file_ to filtered_ | print files
-sed 's/file_/filtered_/' | xargs ls
+# sed 's/file_/filtered_/'
+
+grep -ol "sample" dataset1/file* | xargs -I{} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then size=$(stat --printf="%s" "{}"); echo "$count $size $(basename "{}")"; fi' | sort -k1,1nr -k2,2nr | sed 's/file_/filtered_/'
 
 exit
