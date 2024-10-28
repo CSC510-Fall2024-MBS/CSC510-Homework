095dd41fb7d1f02125085c2e95c90dccf39346d4
Manav Kamdar <80146150+manavkamdar27@users.noreply.github.com>
2024-10-28 18:17:56 -0400

    Updated task3.sh
    Resolved error in final command

--- task3.sh
+++ task3.sh
@@ -24,6 +24,5 @@
 # gawk -F, '{ ageSum += $7; counter += 1 } END { print ageSum / counter }'
 
 # All the parts piped together
-grep -E "^[0-9]{1,3},[0-9],2,".*",.*,.*,.*,.*,.*,.*,.*,S" titanic.csv | sed 's/female/F/; s/male/M/' | gawk -F, '{ print; ageSum += $7; counter += 1 } END { print "\nAverage Age: " ageSum / counter }'
-
+grep -E "^[0-9]{1,3},[0-9],2,.*,.*,.*,.*,.*,.*,.*,.*,S" titanic.csv | sed 's/female/F/; s/male/M/' | gawk -F, '$7 ~ /^[0-9]+(\.[0-9]+)?$/ { print; ageSum += $7; counter += 1 } END { if (counter > 0) print "\nAverage Age: " ageSum / counter; else print "No valid data found" }'
 exit
